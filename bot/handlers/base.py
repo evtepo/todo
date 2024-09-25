d392bc6538fe23013099bd9d0aa7c689f4e1e677
evtepo <andrey_laguta00@mail.ru>
2024-09-25 11:23:24 +1000

    start handler

---
+++ bot/handlers/base.py
@@ -0,0 +1,27 @@
+from aiogram import Bot, Router
+from aiogram.filters import Command, CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.types import BotCommand, CallbackQuery, Message
+from aiogram_dialog import DialogManager
+
+from keyboards.menu import get_menu
+from handlers.auth import login_into_account, register_account
+from handlers.task import get_tasks
+
+
+router = Router(name=__name__)
+
+
+@router.message(CommandStart())
+async def command_start(message: Message):
+    await message.answer(f"Hi, {message.from_user.full_name}")
+
+
+async def set_commands(bot: Bot):
+    commands = (
+        BotCommand(command="register", description="Register an account"),
+        BotCommand(command="login", description="Log into account"),
+        BotCommand(command="tags", description="View tags"),
+        BotCommand(command="task", description="View your tasks"),
+    )
+    await bot.set_my_commands(commands)
