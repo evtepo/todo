a5d4834193ab8eb187ad73fe2ca27a771e81fe20
evtepo <andrey_laguta00@mail.ru>
2024-09-25 11:20:05 +1000

    change password length

--- todo_service/tasks/utils.py
+++ todo_service/tasks/utils.py
@@ -4,4 +4,4 @@ from time import time_ns
 
 def generate_id(data: str) -> str:
     cur_time = time_ns()
-    return sha256(f"{data}{cur_time}".encode()).hexdigest()
+    return sha256(f"{data}{cur_time}".encode()).hexdigest()[:20]
